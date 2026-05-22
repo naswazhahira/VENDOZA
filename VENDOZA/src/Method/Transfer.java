3964efafa2809df8d1bbda68ecc511d1afe4c40c
badzlinaatika <badzlinaatika25@gmail.com>
2026-05-22 13:34:58 +0700

    Update Transfer.java
    menambahkan status pembayaran

--- VENDOZA/src/Method/Transfer.java
+++ VENDOZA/src/Method/Transfer.java
@@ -3,23 +3,32 @@ package Method;
 public class Transfer implements Payment{
     private String namaBank;
     private String nomorRekening;
+    private boolean sudahDibayar;
 
     public Transfer(String namaBank, String nomorRekening) {
         this.namaBank       = namaBank;
         this.nomorRekening  = nomorRekening;
+        this.sudahDibayar = false;
     }
 
     @Override
     public void bayar(double total) {
-        System.out.println(">>> Pembayaran via Transfer Bank <<<");
+        System.out.println(">>> METODE PEMBAYARAN TRANSFER BANK <<<");
         System.out.printf("    Bank    : %s%n", namaBank);
         System.out.printf("    No. Rek : %s%n", nomorRekening);
         System.out.printf("    Nominal : Rp %,.0f%n", total);
-        System.out.println("    Silakan transfer dalam 1x24 jam.");
+        System.out.println("   Silahkan melakukan pembayaran dalam 1x24 jam.");
+        this.sudahDibayar = false;
     }
 
     @Override
     public void konfirmasi() {
-        System.out.println("    [Transfer] Konfirmasi dikirim ke tim verifikasi.");
+        this.sudahDibayar = true; //status diubah menjadi lunas
+        System.out.println("    Transfer diKonfirmasi pembayaran diterima. Pesanan anda sedang diproses");
     }
+
+    @Override
+    public String getStatus() {
+        return sudahDibayar ? "PEMBAYARAN BERHASIL!" : "MENUNGGU PEMBAYARAN!";  //jika sudah melakukan pembayaran(true) akan berhasil dan jika blm membayar(false) akan muncul menunggu pembayaran.
+        }
 }

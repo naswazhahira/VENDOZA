6bc05e873781c327f7bfb7de6e123d2fbabdf4f2
ysjltaa <yesisinaga18@gmail.com>
2026-05-22 13:21:14 +0700

    Enhance ItemKeranjang with quantity management methods
    Added methods to modify item quantity in the cart.

--- VENDOZA/src/Produk/ItemKeranjang.java
+++ VENDOZA/src/Produk/ItemKeranjang.java
@@ -1,6 +1,7 @@
 package Produk;
 
 public class ItemKeranjang {
+    // ENCAPSULATION
     private ProdukVendoza produk;
     private int jumlah;
 
@@ -17,10 +18,23 @@ public class ItemKeranjang {
         return jumlah;
     }
 
+    // 1. Method untuk menambahkan kuantitas jika user memilih produk yang sama lagi
+    public void tambahJumlah(int jumlah) {
+        this.jumlah += jumlah;
+    }
+
+    // 2. Method untuk mengubah jumlah secara langsung
+    public void setJumlah(int jumlah) {
+        if (jumlah <= 0) throw new IllegalArgumentException("Jumlah item harus lebih dari 0!");
+        this.jumlah = jumlah;
+    }
+
     public double getSubtotal() {
         return produk.getHarga() * jumlah;
     }
 
+    // POLYMORPHISM (OVERRIDING)
+    // Melakukan override method toString() dari kelas Object Java.
     @Override
     public String toString() {
         return String.format("  - %-22s x%d = Rp %,.0f",
